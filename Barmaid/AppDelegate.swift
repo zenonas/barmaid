cebf75c8e113c95e31b2ef5fe80e46d3777f8df7
Zen Kyprianou <zen@kyprianou.eu>
2014-07-14 10:14:18 +0100

    Add xcode project

---
+++ Barmaid/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Barmaid
+//
+//  Created by Zen Kyprianou on 14/07/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Barmaid/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Barmaid
+//
+//  Created by Zen Kyprianou on 14/07/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
