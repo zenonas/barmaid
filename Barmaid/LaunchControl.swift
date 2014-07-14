c5934189bbaa40f98d88f9c354e9b8648ca89cb8
Zen Kyprianou <zen@kyprianou.eu>
2014-07-14 17:07:22 +0100

    Start working on the Launch Control class

---
+++ Barmaid/LaunchControl.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchControl.swift
+//  Barmaid
+//
+//  Created by Zen Kyprianou on 14/07/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+import Cocoa
+
+class LaunchControl {
+
+    var services: Dictionary<String, String>
+    var launchctl: NSTask
+    
+    init() {
+        self.services = Dictionary<String, String>()
+        self.launchctl = NSTask()
+        self.launchctl.launchPath = "/bin/launchctl"
+    }
+    
+    func load(services: Dictionary<String, String>) {
+        for (serviceName, serviceLocation) in services {
+            self.launchctl.arguments = ["load", serviceLocation]
+            self.launchctl.launch()
+            self.launchctl.waitUntilExit()
+        }
+    }
+    
+}
