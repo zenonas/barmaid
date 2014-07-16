ea99e1fad2f30d8ed618b030361c88c4de508bab
Zen Kyprianou <zen@kyprianou.eu>
2014-07-16 18:24:59 +0100

    Update ui to a more slicker variation

--- Barmaid/BarmaidPopoverViewController.swift
+++ Barmaid/BarmaidPopoverViewController.swift
@@ -19,6 +19,10 @@ class BarmaidPopoverViewController: NSViewController, NSTableViewDelegate, NSTab
         
     }
     
+    override func viewDidAppear() {
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
@@ -28,24 +32,47 @@ class BarmaidPopoverViewController: NSViewController, NSTableViewDelegate, NSTab
         return self.homebrew.services.count
     }
     
-    @IBAction func startButtonPress(sender: NSButton) {
+    @IBAction func barmaidAboutButon(sender: NSTextField) {
+        NSApp.orderFrontStandardAboutPanel(sender)
+    }
+    
+    @IBAction func startStopButtonPress(sender: NSButton) {
         var row = self.tableView.rowForView(sender)
         var service: Service = self.homebrew.services.objectAtIndex(row) as Service
-        service.start()
-        println(service.status())
+        if sender.identifier == "start" {
+            if service.status() == "stopped" {
+                service.start()
+                sender.identifier = "stop"
+                sender.image = NSImage(named: NSImageNameStopProgressTemplate)
+            }
+        }
+        else if sender.identifier == "stop" {
+            if service.status() == "running" {
+                service.stop()
+                sender.identifier = "start"
+                sender.image = NSImage(named: NSImageNameRightFacingTriangleTemplate)
+            }
+            
+        }
+        self.tableView.reloadData()
     }
     
     @IBAction func restartButtonPress(sender: NSTableView) {
         var row = self.tableView.rowForView(sender)
         var service: Service = self.homebrew.services.objectAtIndex(row) as Service
-        service.stop()
-        service.start()
+        service.restart()
     }
     
     func tableView(tableView: NSTableView!, viewForTableColumn tableColumn: NSTableColumn!, row: Int) -> NSView! {
         var service: Service = self.homebrew.services[row] as Service
         var cellView: NSTableCellView = tableView.makeViewWithIdentifier("ServiceCell", owner: self) as NSTableCellView
         cellView.textField.stringValue = service.name
+        if service.status() == "running" {
+            cellView.imageView.image = NSImage(named:NSImageNameStatusAvailable)
+        } else {
+            cellView.imageView.image = NSImage(named: NSImageNameStatusUnavailable)
+        }
         return cellView
     }
+
 }
