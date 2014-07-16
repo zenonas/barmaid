d4a244b918a29b96e5bb59e5dc22e33cb24d743f
Zen Kyprianou <zen@kyprianou.eu>
2014-07-16 10:49:30 +0100

    Wire buttons to services

--- Barmaid/BarmaidPopoverViewController.swift
+++ Barmaid/BarmaidPopoverViewController.swift
@@ -10,8 +10,11 @@ import Cocoa
 
 class BarmaidPopoverViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
+    @IBOutlet var tableView: NSTableView
+    @IBOutlet var startButton: NSButton
+    @IBOutlet var restartButton: NSButton
     var homebrew: Homebrew = Homebrew()
-    
+
     override func awakeFromNib() {
         
     }
@@ -25,10 +28,25 @@ class BarmaidPopoverViewController: NSViewController, NSTableViewDelegate, NSTab
         return self.homebrew.services.count
     }
     
+    @IBAction func startButtonPress(sender: NSButton) {
+        var row = self.tableView.rowForView(sender)
+        var service: Service = self.homebrew.services.objectAtIndex(row) as Service
+        service.start()
+    }
+    
+    @IBAction func restartButtonPress(sender: NSTableView) {
+        var row = self.tableView.rowForView(sender)
+        var service: Service = self.homebrew.services.objectAtIndex(row) as Service
+        service.stop()
+        service.start()
+    }
+    
     func tableView(tableView: NSTableView!, viewForTableColumn tableColumn: NSTableColumn!, row: Int) -> NSView! {
         var service: Service = self.homebrew.services[row] as Service
         var cellView: NSTableCellView = tableView.makeViewWithIdentifier("ServiceCell", owner: self) as NSTableCellView
         cellView.textField.stringValue = service.name
+        //var startButton: NSButton = tableView.makeViewWithIdentifier("startButton", owner: self) as NSButton
+        //startButton.stringValue = "Hello"
         return cellView
     }
 }
