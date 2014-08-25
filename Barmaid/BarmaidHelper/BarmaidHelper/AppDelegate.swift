64952d5fc32c378afc94b448994eac41542815c7
Zen Kyprianou <zen@kyprianou.eu>
2014-08-25 14:49:33 +0100

    Add a barmaid helper project

---
+++ Barmaid/BarmaidHelper/BarmaidHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BarmaidHelper
+//
+//  Created by Zen Kyprianou on 25/08/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Barmaid/BarmaidHelper/BarmaidHelper/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BarmaidHelper
+//
+//  Created by Zen Kyprianou on 25/08/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
